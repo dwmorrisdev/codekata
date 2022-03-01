18af8f81d3f118a90984cd5b678cd82a93f0f97e
Dustin <dwmorris.biz@outlook.com>
2022-03-01 15:03:34 -0600

    Updates to add labels for existing tests and adds kata_04

---
+++ kata_04/kata.js
@@ -0,0 +1,24 @@
+// Multiplication Tables
+
+// TASK: Print a table containing multiplication tables
+
+// Print a table that contains all the answers to the
+// multiplication tables for a given x & y ?
+
+// If input is invalid, return an empty array
+
+
+function myFunction(x, y) {
+  // example input, 5, 5
+  // will return 5 x 5, with the answers for the give multiplication matrix
+  // sample return
+  // [
+  //   [1, 2, 3, 4, 5],
+  //   [2, 4, 6, 8, 10], 
+  //   [3, 6, 9, 12, 15],
+  //   [4, 8, 12, 16, 20],
+  //   [5, 10, 15, 20, 25],
+  // ]
+  return [];
+}
+module.exports = myFunction;
